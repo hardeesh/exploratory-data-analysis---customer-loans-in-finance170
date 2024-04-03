c6b0b36f58b63281c657063a2bf0958d06262361
Hardeesh <hardeesh.s@gmail.com>
2024-04-03 19:42:06 +0100

    Makes df a paramater to use for different dataframes

--- DataFrameTransform.py
+++ DataFrameTransform.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas as pd
 
 
 class DataFrameTransform:
     def __init__(self, df):
         self.df = df
+
+    def logtransform(self, target_df : pd.DataFrame, column : str):
+        log = target_df[[column]].map(lambda i: np.log(i) if i > 0 else 0)
+        return log
     
-    def logtransform(self, column):
-        log = self.df[column].map(lambda i: np.log(i) if i > 0 else 0)
-        return (log)
+    
